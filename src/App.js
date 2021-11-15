47304db8dca882829e6ec394b6f5473ce046b87b
ldulivo <ldulivo@gmail.com>
2021-11-15 19:12:37 -0300

    Basic blog.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import ArticlesList from './pages/ArticlesList';
+import Article from './pages/Article';
+import NotFound from "./pages/NotFound";
+
+// Components
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/articles-list" element={<ArticlesList />} />
+          <Route exact path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">Hola, Bienvenido a mi blog</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, assumenda? Dolore tenetur sequi minima at provident a explicabo perferendis quo minus. Laboriosam, voluptatem sequi iusto sed ex deserunt minima esse illum unde, qui nemo inventore facere fugit.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, assumenda? Dolore tenetur sequi minima at provident a explicabo perferendis quo minus. Laboriosam, voluptatem sequi iusto sed ex deserunt minima esse illum unde, qui nemo inventore facere fugit.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, assumenda? Dolore tenetur sequi minima at provident a explicabo perferendis quo minus. Laboriosam, voluptatem sequi iusto sed ex deserunt minima esse illum unde, qui nemo inventore facere fugit. Minus asperiores consectetur accusantium ut quae blanditiis doloribus, iusto ab, dicta quasi tempore temporibus animi, quam in nemo unde tempora maxime debitis. Ipsam suscipit repellat totam praesentium nesciunt facere. Accusamus dolor doloremque repellendus eius voluptates, placeat doloribus? Nemo sequi magnam modi rerum! Architecto facilis omnis veritatis, commodi et tempore incidunt sit esse cupiditate iste eum ipsum, voluptatibus nulla, atque sunt possimus magni distinctio?</p>
+        </div>
+    )
+}
+
+export default Home;
